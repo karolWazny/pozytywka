59c99ad6c260d4af9a53324e1c7c9150173ce8d4
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-04 00:49:00 +0200

    added files

---
+++ saw.vhd
@@ -0,0 +1,88 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:49:39 03/31/2022 
+-- Design Name: 
+-- Module Name:    saw - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity saw is
+    Port ( Clk_50MHz : in  STD_LOGIC;
+           Start : out  STD_LOGIC;
+           Cmd : out  STD_LOGIC_VECTOR (3 downto 0);
+           Addr : out  STD_LOGIC_VECTOR (3 downto 0);
+           DATA : out  STD_LOGIC_VECTOR (11 downto 0));
+end saw;
+
+architecture Behavioral of saw is
+	signal count : INTEGER RANGE 0 TO 16384; --tu zale¿nie od obs³ugiwanych czêstotliwoœci trzeba ustawiæ zakres
+	signal output_value : INTEGER RANGE 0 TO 15;
+	type deriv_state is ('A', 'B', 'C');
+	signal deriv, next_deriv : deriv_state;
+	
+begin
+	process (Clk_50MHz)
+	BEGIN
+		count <= count;
+		IF (rising_edge(Clk_50MHz)) THEN
+			count <= count + 1;
+		END IF;
+		IF (count = 4167) THEN
+			count <= 0;
+			IF (output_value = 15) THEN
+				output_value <= 0;
+			ELSE
+				output_value <= output_value + 1;
+			END IF;
+		END IF;
+		DATA <= std_logic_vector(to_unsigned(output_value, 4)) & "00000000";
+	END PROCESS;
+	
+	process(deriv, count)
+	BEGIN
+		next_deriv <= deriv;
+		case deriv is
+			when 'A' =>
+				next_deriv <= 'B';
+			when 'B' =>
+				next_deriv <= 'C';
+			when 'C' =>
+				if count = 0 then
+					next_deriv <= 'A';
+				end if;
+		end case;
+	END PROCESS;
+	
+	process(Clk_50MHz)
+	BEGIN
+		deriv <= next_deriv;
+	END PROCESS;
+	
+	Start <= '1' when deriv = 'B' or deriv = 'A' else '0';
+	Cmd <= "0000";
+	Addr <= "0000";
+end Behavioral;
+ 
---
+++ saw_test.vhd
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   13:13:23 03/31/2022
+-- Design Name:   
+-- Module Name:   C:/Users/lab/Documents/Visual Studio Projects/pozytywka/saw_test.vhd
+-- Project Name:  pozytywka
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: saw
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY saw_test IS
+END saw_test;
+ 
+ARCHITECTURE behavior OF saw_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT saw
+    PORT(
+         Clk_50MHz : IN  std_logic;
+         Start : OUT  std_logic;
+         Cmd : OUT  std_logic_vector(3 downto 0);
+         Addr : OUT  std_logic_vector(3 downto 0);
+         DATA : OUT  std_logic_vector(11 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Clk_50MHz : std_logic := '0';
+
+ 	--Outputs
+   signal Start : std_logic;
+   signal Cmd : std_logic_vector(3 downto 0);
+   signal Addr : std_logic_vector(3 downto 0);
+   signal DATA : std_logic_vector(11 downto 0);
+
+   -- Clock period definitions
+   constant Clk_50MHz_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: saw PORT MAP (
+          Clk_50MHz => Clk_50MHz,
+          Start => Start,
+          Cmd => Cmd,
+          Addr => Addr,
+          DATA => DATA
+        );
+
+   -- Clock process definitions
+   Clk_50MHz_process :process
+   begin
+		Clk_50MHz <= '0';
+		wait for Clk_50MHz_period/2;
+		Clk_50MHz <= '1';
+		wait for Clk_50MHz_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for Clk_50MHz_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
